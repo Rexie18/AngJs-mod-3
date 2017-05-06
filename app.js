fe4ada7c40b62e970b2537b7adee0f45544e8315
Rexie18 <ro@students.kiron.ngo>
2017-05-06 22:26:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,69 +1,86 @@
-(function () {
+(function(){
 'use strict';
 
-angular.module('NarrowItDownApp', [])
-.controller('MenuCategoriesController', MenuCategoriesController)
-.service('MenuCategoriesService', MenuCategoriesService)
-.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+angular.module('NarrowItDownApp',[])
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.directive('foundItems', FoundDirective);
 
 
-MenuCategoriesController.$inject = ['MenuCategoriesService'];
-function MenuCategoriesController(MenuCategoriesService) {
-  var searchMenu = this;
+function FoundDirective() {
+  var ddo = {
+    templateUrl:'component/list.html',
+    scope: {
+      check:"<checkAny",
+      searchResult:"<foundList",
+      onRemove : "&"
+    }
+  }
 
-    searchMenu.logMenuItems = function (enteredValue) {
-    var promise = MenuCategoriesService.getMenuForCategory(enteredValue);
-
-    promise.then(function (response) {
-      searchMenu.result = response.data;
-    })
-    .catch(function (error) {
-      return("Please Enter Menu Item.");
-    })
-  };
-
-  searchMenu.remove = function (itemIndex){
-  MenuCategoriesService.remove(itemIndex)
- };
-  
+  return ddo
 }
 
+NarrowItDownController.$inject=['MenuSearchService']
+function NarrowItDownController (MenuSearchService) {
+    var search = this;
+    search.searchTerm = "";
+    search.nothing = false;
 
-MenuCategoriesService.$inject = ['$http', 'ApiBasePath'];
-function MenuCategoriesService($http, ApiBasePath) {
-  var service = this;
-
-  
-  service.getMenuForCategory = function (enteredValue) {
-    var response = $http({
-      method: "GET",
-      url: (ApiBasePath + "/menu_items.json"),
-      params: {
-        category: enteredValue
+    search.getList = function(){
+      if(search.searchTerm.length == 0) {
+        search.nothing = true;
+        search.found = [];
+      } else {
+         MenuSearchService.getMatchedMenuItems(search.searchTerm)
+         .then(function(response){
+           if (response.length==0) {
+             search.nothing = true;
+           } else{
+             search.nothing = false;
+           }
+           search.found = response;
+         });
       }
-	   
+    };
+
+    search.remove = function(index) {
+      search.found = MenuSearchService.remove(index);
+    };
+}
+
+MenuSearchService.$inject = ['$http']
+function MenuSearchService ($http) {
+  var service = this;
+  var foundItems = [];
+
+  service.getMatchedMenuItems = function (searchTerm) {
+    var response = $http({
+      method:"GET",
+      url:"https://davids-restaurant.herokuapp.com/menu_items.json"
     });
 
-    return response;
+    foundItems = [];
+
+    return response.then(function(response){
+      var menus = response.data.menu_items;
+      for (var i=0;i<menus.length;i++) {
+        var menuDesc = menus[i].description.toLowerCase()
+        if (menuDesc.includes(searchTerm.toLowerCase())) {
+          foundItems.push(menus[i]);
+        }
+      }
+      return foundItems;
+    })
+    .catch(function(error){
+      console.log("Something went wrong~!")
+    });
   };
 
-  service.remove = function(itemIndex){
-		service.response.splice(itemIndex,1);
-		};
+  service.remove = function(index) {
+    foundItems.splice(index,1);
+    return foundItems;
+  };
+
 }
 
 })();
-
-
-// http://plnkr.co/edit/452nFDmxnCROsM4m4Ylf?p=preview
-
-
-// app.controller('MyController', function($http) {
-//     var vm = this;
-//     vm.mydata = [];
-
-//     $http.get(URI)
-//         .then(function(result) {
-//           console.log(result);
-//           vm.mydata = result.data;
-//          });
