be09d7a1dfa28ea3f2d52c961847470699380a36
Rexie18 <ro@students.kiron.ngo>
2017-05-08 16:41:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -15,25 +15,25 @@ function FoundDirective() {
       searchResult:"<foundList",
       onRemove : "&"
     }
-  }
+  };
 
-  return ddo
+  return ddo;
 }
 
-NarrowItDownController.$inject=['MenuSearchService']
+NarrowItDownController.$inject=['MenuSearchService'];
 function NarrowItDownController (MenuSearchService) {
     var search = this;
     search.searchTerm = "";
     search.nothing = false;
 
     search.getList = function(){
-      if(search.searchTerm.length == 0) {
+      if(search.searchTerm.length === 0) {
         search.nothing = true;
         search.found = [];
       } else {
          MenuSearchService.getMatchedMenuItems(search.searchTerm)
          .then(function(response){
-           if (response.length==0) {
+           if (response.length===0) {
              search.nothing = true;
            } else{
              search.nothing = false;
@@ -48,7 +48,7 @@ function NarrowItDownController (MenuSearchService) {
     };
 }
 
-MenuSearchService.$inject = ['$http']
+MenuSearchService.$inject = ['$http'];
 function MenuSearchService ($http) {
   var service = this;
   var foundItems = [];
@@ -64,7 +64,7 @@ function MenuSearchService ($http) {
     return response.then(function(response){
       var menus = response.data.menu_items;
       for (var i=0;i<menus.length;i++) {
-        var menuDesc = menus[i].description.toLowerCase()
+        var menuDesc = menus[i].description.toLowerCase();
         if (menuDesc.includes(searchTerm.toLowerCase())) {
           foundItems.push(menus[i]);
         }
@@ -72,7 +72,7 @@ function MenuSearchService ($http) {
       return foundItems;
     })
     .catch(function(error){
-      console.log("Something went wrong~!")
+      console.log("Something went wrong~!");
     });
   };
 
@@ -84,3 +84,90 @@ function MenuSearchService ($http) {
 }
 
 })();
+
+// (function(){
+// 'use strict';
+
+// angular.module('NarrowItDownApp',[])
+// .controller('NarrowItDownController', NarrowItDownController)
+// .service('MenuSearchService', MenuSearchService)
+// .directive('foundItems', FoundDirective);
+
+
+// function FoundDirective() {
+//   var ddo = {
+//     templateUrl:'https://davids-restaurant.herokuapp.com/component/list.html',
+//     scope: {
+//       check:"<checkAny",
+//       searchResult:"<foundList",
+//       onRemove : "&"
+//     }
+//   }
+
+//   return ddo
+// }
+
+// NarrowItDownController.$inject=['MenuSearchService']
+// function NarrowItDownController (MenuSearchService) {
+//     var search = this;
+//     search.searchTerm = "";
+//     search.nothing = false;
+
+//     search.getList = function(){
+//       if(search.searchTerm.length == 0) {
+//         search.nothing = true;
+//         search.found = [];
+//       } else {
+//          MenuSearchService.getMatchedMenuItems(search.searchTerm)
+//          .then(function(response){
+//            if (response.length==0) {
+//              search.nothing = true;
+//            } else{
+//              search.nothing = false;
+//            }
+//            search.found = response;
+//          });
+//       }
+//     };
+
+//     search.remove = function(index) {
+//       search.found = MenuSearchService.remove(index);
+//     };
+// }
+
+// MenuSearchService.$inject = ['$http']
+// function MenuSearchService ($http) {
+//   var service = this;
+//   var foundItems = [];
+
+//   service.getMatchedMenuItems = function (searchTerm) {
+//     var response = $http({
+//       method:"GET",
+//       url:"https://davids-restaurant.herokuapp.com/menu_items.json"
+//     });
+
+//     foundItems = [];
+
+//     return response.then(function(response){
+//       var menus = response.data.menu_items;
+//       for (var i=0;i<menus.length;i++) {
+//         var menuDesc = menus[i].description.toLowerCase()
+//         if (menuDesc.includes(searchTerm.toLowerCase())) {
+//           foundItems.push(menus[i]);
+//         }
+//       }
+//       return foundItems;
+//     })
+//     .catch(function(error){
+//       console.log("Something went wrong~!")
+//     });
+//   };
+
+//   service.remove = function(index) {
+//     foundItems.splice(index,1);
+//     return foundItems;
+//   };
+
+// }
+
+// })();
